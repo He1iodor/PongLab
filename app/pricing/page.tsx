629ad0e29cba06ce878c842d5dba31ee891d1a20
He1iodor <nuzhdov17@gmail.com>
2026-06-02 22:42:09 +0300

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,3 +1,8 @@
-export default function PricingPage() {
-  return <div>Pricing</div>;
+export default function Page() {
+  return (
+    <div>
+      <h1>Pricing</h1>
+      <p>Тарифы</p>
+    </div>
+  )
 }
