49d5cbe7c736551fb9e48fe4cba5057053195c21
He1iodor <nuzhdov17@gmail.com>
2026-06-03 23:28:33 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -98,8 +98,8 @@ export default function Navbar() {
               <Image
                 src="/logo.png"
                 alt="PongLab"
-                width={42}
-                height={42}
+                width={160}
+                height={60}
                 priority
               />
 
