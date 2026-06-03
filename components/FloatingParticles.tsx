d5b97d2f7da95c3f8cc5e201d777b2b15e2bf538
He1iodor <nuzhdov17@gmail.com>
2026-06-04 00:43:07 +0300

    Update FloatingParticles.tsx

--- components/FloatingParticles.tsx
+++ components/FloatingParticles.tsx
@@ -1,36 +1,48 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function FloatingParticles({ mousePosition }: any) {
+  const [screenWidth, setScreenWidth] = useState(0);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+  }, []);
+
   const particles = Array.from({ length: 20 });
 
   return (
     <>
-      {particles.map((_, i) => (
-        <motion.div
-          key={i}
-          className="absolute rounded-full bg-[#8F5BFF] opacity-20"
-          style={{
-            width: Math.random() * 6 + 2,
-            height: Math.random() * 6 + 2,
-            left: `${Math.random() * 100}%`,
-            top: `${Math.random() * 100}%`,
-          }}
-          animate={{
-            y: [0, -60, 0],
-            x: mousePosition
-              ? (mousePosition.x - window.innerWidth / 2) * 0.0005
-              : 0,
-            opacity: [0.1, 0.4, 0.1],
-          }}
-          transition={{
-            duration: 8 + Math.random() * 8,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-        />
-      ))}
+      {particles.map((_, i) => {
+        const size = Math.random() * 6 + 2;
+
+        return (
+          <motion.div
+            key={i}
+            className="absolute rounded-full bg-[#8F5BFF] opacity-20"
+            style={{
+              width: size,
+              height: size,
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+            }}
+            animate={{
+              y: [0, -60, 0],
+              x:
+                mousePosition && screenWidth
+                  ? (mousePosition.x - screenWidth / 2) * 0.0005
+                  : 0,
+              opacity: [0.1, 0.4, 0.1],
+            }}
+            transition={{
+              duration: 8 + Math.random() * 8,
+              repeat: Infinity,
+              ease: "easeInOut",
+            }}
+          />
+        );
+      })}
     </>
   );
 }
