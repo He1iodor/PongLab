5c3761c3891ffd51c9129611c960dd57a8b5d9b8
He1iodor <nuzhdov17@gmail.com>
2026-06-04 16:06:11 +0300

    Update Hero.tsx

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -115,8 +115,8 @@ export default function Hero() {
               <Image
                 src="/bot.png"
                 alt="Robot Training"
-                width={400}
-                height={400}
+                width={300}
+                height={300}
                 className="w-full h-auto drop-shadow-[0_0_120px_rgba(107,48,206,.6)]"
               />
             </div>
