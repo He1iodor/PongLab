da266fcaea5cef92a85f88056c2aef313a207976
He1iodor <nuzhdov17@gmail.com>
2026-06-04 16:47:37 +0300

    Update Hero.tsx

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -107,7 +107,7 @@ export default function Hero() {
 
           {/* RIGHT */}
           <div className="hidden lg:flex justify-center items-center">
-            <div className="relative w-full max-w-[650px]">
+            <div className="relative w-full max-w-[650px] translate-x-12 translate-y-10 lg:translate-x-20 lg:translate-y-16">
               <Image
   src="/bot.png"
   alt="Robot Training"
