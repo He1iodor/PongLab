ed4392432c2ea639f45659d07f57e3eda659a094
He1iodor <nuzhdov17@gmail.com>
2026-06-05 13:25:24 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -137,7 +137,7 @@ export default function Navbar() {
               "
             >
               <span className="absolute inset-0 bg-gradient-to-r from-transparent via-white/20 to-transparent -translate-x-full hover:translate-x-full transition-all duration-700" />
-              <span className="relative z-10">Начать бесплатно</span>
+              <span className="relative z-10">Личный кабинет</span>
             </button>
 
             <button
