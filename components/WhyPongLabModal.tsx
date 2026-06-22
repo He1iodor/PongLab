f7fcb64bce6ddd49cd4339930033cf905a72ee3f
He1iodor <nuzhdov17@gmail.com>
2026-06-22 09:14:36 +0300

    Update WhyPongLabModal.tsx

--- components/WhyPongLabModal.tsx
+++ components/WhyPongLabModal.tsx
@@ -29,113 +29,130 @@ export default function EffectivenessModal({
   }, [open, onClose]);
 
   if (!open) return null;
-  
+
   return (
-  <div
-    className="
-      fixed inset-0
-      z-[9999]
-      bg-black/80
-      backdrop-blur-xl
-      flex items-center justify-center
-      p-4
-    "
-    onClick={onClose}
-  >
-
     <div
-      onClick={(e) => e.stopPropagation()}
+      onClick={onClose}
       className="
-        relative
-        w-[96%]
-        max-w-[1800px]
-        max-h-[92vh]
-        overflow-y-auto
-        rounded-[32px]
-        bg-[#0B0D18]
-        border border-white/10
-        text-white
-
-        pt-20
-        px-8
-        pb-10
-
-        lg:pt-24
-        lg:px-14
-        lg:pb-14
+        fixed inset-0
+        z-[9999]
+        bg-black/80
+        backdrop-blur-xl
+        flex items-center justify-center
+        p-4
       "
     >
-
-      {/* PURPLE GLOW */}
       <div
+        onClick={(e) => e.stopPropagation()}
         className="
-          absolute
-          top-[15%]
-          left-[50%]
-          -translate-x-1/2
-          w-[900px]
-          h-[900px]
-          rounded-full
-          bg-[#8F5BFF]/15
-          blur-[180px]
-          pointer-events-none
-        "
-      />
+          relative
+          w-[98%]
+          max-w-[1800px]
+          h-[92vh]
+          overflow-y-auto
 
-      {/* BLUE GLOW */}
-      <div
-        className="
-          absolute
-          top-[40%]
-          right-[-150px]
-          w-[600px]
-          h-[600px]
-          rounded-full
-          bg-[#4A7DFF]/10
-          blur-[150px]
-          pointer-events-none
-        "
-      />
+          rounded-[32px]
 
-      {/* Крестик */}
-      <button
-        onClick={onClose}
-        className="
-          absolute
-          top-6
-          right-6
-          z-20
-          h-12
-          w-12
-          rounded-xl
-          border
-          border-white/10
-          bg-white/5
-          flex
-          items-center
-          justify-center
-          hover:bg-white/10
-          transition
+          bg-[#090B18]
+          border border-white/10
+
+          text-white
+
+          pt-20
+          px-8
+          pb-10
+
+          lg:pt-24
+          lg:px-14
+          lg:pb-14
         "
       >
-        <X size={20} />
-      </button>
-
-      {/* Контент */}
-      <div className="relative z-10">
-
+        {/* Purple Glow */}
         <div
           className="
-            rounded-[24px]
-            border border-white/10
-            bg-white/[0.03]
-            h-[600px]
+            absolute
+            top-[10%]
+            left-1/2
+            -translate-x-1/2
+
+            w-[900px]
+            h-[900px]
+
+            rounded-full
+            bg-[#8F5BFF]/15
+
+            blur-[180px]
+
+            pointer-events-none
           "
         />
 
+        {/* Blue Glow */}
+        <div
+          className="
+            absolute
+            bottom-0
+            right-[-150px]
+
+            w-[600px]
+            h-[600px]
+
+            rounded-full
+            bg-[#4A7DFF]/10
+
+            blur-[150px]
+
+            pointer-events-none
+          "
+        />
+
+        {/* Close Button */}
+        <button
+          onClick={onClose}
+          className="
+            absolute
+            top-6
+            right-6
+            z-50
+
+            h-12
+            w-12
+
+            rounded-xl
+
+            border
+            border-white/10
+
+            bg-white/5
+
+            flex
+            items-center
+            justify-center
+
+            hover:bg-white/10
+
+            transition
+          "
+        >
+          <X size={20} />
+        </button>
+
+        {/* Content */}
+        <div className="relative z-10">
+          <div
+            className="
+              h-[600px]
+
+              rounded-[24px]
+
+              border
+              border-white/10
+
+              bg-white/[0.03]
+            "
+          />
+        </div>
       </div>
-
     </div>
-
-  </div>
-);
+  );
+}
